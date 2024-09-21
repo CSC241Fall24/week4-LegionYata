4fc830dc179776a73b6ab0f7b7508f4cda3a9d8b
LegionYata <ly996437@wcupa.edu>
2024-09-21 15:42:07 -0400

    Update

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,9 +1,45 @@
 // Problem1.java
 public class Problem1 {
+
     public static ListNode concatenate(ListNode l1, ListNode l2) {
         // TODO: Implement the concatenate method
         // This method should concatenate l2 to l1 and return l1
         // Remember to create new nodes instead of directly linking l1 to l2
-        return null;
+        ListNode first = l1;
+        ListNode second = l2;
+
+
+        if (l1 != null) {
+       while (first.next != null) {
+            first = first.next;
+        }
+        first.next = second;
+        }
+
+
+
+        // Handle the case where l1 is null (empty list)
+        if (l1 == null) {
+            // Create a new list by copying nodes from l2
+            ListNode newHead = null;
+            ListNode current = null;
+            while (l2 != null) {
+                if (newHead == null) {
+                    newHead = new ListNode(l2.val);
+                    current = newHead;
+                } else {
+                    current.next = new ListNode(l2.val);
+                    current = current.next;
+                }
+                l2 = l2.next;
+            }
+            return newHead;
+        }
+
+
+        return l1;
     }
+
+
 }
+
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,9 +1,26 @@
-
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
-        // TODO: Implement the insert method
-        // This method should insert a new node with the given value at the specified position
-        // If the position is out of bounds, insert the node at the end of the list
-        return null;
+        ListNode newNode = new ListNode(val);
+
+        // Handle insertion at the beginning
+        if (position == 1 || head == null) {
+            newNode.next = head;
+            return newNode;
+        }
+
+        ListNode current = head;
+        int currentPosition = 1;
+
+        // Traverse the list to find the insertion point
+        while (current.next != null && currentPosition < position - 1) {
+            current = current.next;
+            currentPosition++;
+        }
+
+        // Insert the new node
+        newNode.next = current.next;
+        current.next = newNode;
+
+        return head;
     }
 }
